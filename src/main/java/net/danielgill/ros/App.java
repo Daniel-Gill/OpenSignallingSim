efb08d9cc185904bee988a7c80adc9a09e71c59e
Daniel-Gill <d.gill@se20.qmul.ac.uk>
2022-04-20 22:24:16 +0100

    Removed unused files

--- src/main/java/net/danielgill/ros/App.java
+++ src/main/java/net/danielgill/ros/App.java
@@ -8,17 +8,10 @@ import com.almasb.fxgl.input.InputModifier;
 
 import javafx.scene.input.MouseButton;
 import net.danielgill.ros.block.Block;
-import net.danielgill.ros.graphics.Grid;
 import net.danielgill.ros.signal.FourAspectSignal;
-import net.danielgill.ros.signal.SignalAspect;
-import net.danielgill.ros.signal.TwoAspectSignal;
-import net.danielgill.ros.track.StraightTrackElement;
 import net.danielgill.ros.ui.DragAction;
-import net.danielgill.ros.ui.LeftClickAction;
-import net.danielgill.ros.ui.RightClickAction;
 
 public class App extends GameApplication {
-    private static Grid grid;
 
     @Override
     protected void initSettings(GameSettings settings) {
@@ -33,10 +26,7 @@ public class App extends GameApplication {
 
     @Override
     protected void initGame() {
-        grid.addElement(new StraightTrackElement(11, 10));
-        grid.addElement(new StraightTrackElement(10, 10));
-        grid.addElement(new StraightTrackElement(10, 11));
-        grid.addElement(new StraightTrackElement(12, 10));
+        
     }
 
     @Override
@@ -46,19 +36,15 @@ public class App extends GameApplication {
     }
 
     private void registerInputs(Input input) {
-        input.addAction(new DragAction("Drag"), MouseButton.SECONDARY, InputModifier.CTRL);
-        input.addAction(new RightClickAction("Right Click"), MouseButton.SECONDARY);
-        input.addAction(new LeftClickAction("Left Click"), MouseButton.PRIMARY);
+        input.addAction(new DragAction("Drag"), MouseButton.SECONDARY);
     }
 
     @Override
     protected void initUI() {
-        //UI ui = FXGL.getAssetLoader().loadUI("main.fxml", new MainController());
-        //FXGL.getGameScene().addUI(ui);
+        
     }
 
     public static void main(String[] args) {
-        grid = Grid.getInstance();
         launch(args);
         Block b = new Block("1", new FourAspectSignal());
         Block b2 = new Block("2", new FourAspectSignal());

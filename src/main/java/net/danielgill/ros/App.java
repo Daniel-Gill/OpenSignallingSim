cc158f4edcb61cdc23823a6533c28a9bbd6543f0
Daniel-Gill <d.gill@se20.qmul.ac.uk>
2022-04-20 14:45:31 +0100

    Added grid entities

--- src/main/java/net/danielgill/ros/App.java
+++ src/main/java/net/danielgill/ros/App.java
@@ -4,14 +4,17 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.InputModifier;
 
 import javafx.scene.input.MouseButton;
 import net.danielgill.ros.graphics.Grid;
 import net.danielgill.ros.track.StraightTrackElement;
 import net.danielgill.ros.ui.DragAction;
+import net.danielgill.ros.ui.LeftClickAction;
+import net.danielgill.ros.ui.RightClickAction;
 
 public class App extends GameApplication {
-    public static Grid GRID = Grid.getInstance();
+    private static Grid grid = Grid.getInstance();
 
     @Override
     protected void initSettings(GameSettings settings) {
@@ -26,11 +29,10 @@ public class App extends GameApplication {
 
     @Override
     protected void initGame() {
-        GRID.addElement(new StraightTrackElement(11, 10));
-        GRID.addElement(new StraightTrackElement(10, 10));
-        GRID.addElement(new StraightTrackElement(10, 11));
-        GRID.addElement(new StraightTrackElement(12, 10));
-        GRID.draw();
+        grid.addElement(new StraightTrackElement(11, 10));
+        grid.addElement(new StraightTrackElement(10, 10));
+        grid.addElement(new StraightTrackElement(10, 11));
+        grid.addElement(new StraightTrackElement(12, 10));
     }
 
     @Override
@@ -40,7 +42,9 @@ public class App extends GameApplication {
     }
 
     private void registerInputs(Input input) {
-        input.addAction(new DragAction("Drag"), MouseButton.SECONDARY);
+        input.addAction(new DragAction("Drag"), MouseButton.SECONDARY, InputModifier.CTRL);
+        input.addAction(new RightClickAction("Right Click"), MouseButton.SECONDARY);
+        input.addAction(new LeftClickAction("Left Click"), MouseButton.PRIMARY);
     }
 
     @Override
--- src/main/java/net/danielgill/ros/graphics/Grid.java
+++ src/main/java/net/danielgill/ros/graphics/Grid.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 
+import javafx.geometry.Point2D;
 import net.danielgill.ros.track.TrackElement;
 
 public class Grid {
@@ -14,6 +15,7 @@ public class Grid {
 
     private static Grid instance = null;
     private ArrayList<TrackElement> gridElements = new ArrayList<>();
+    private ArrayList<Entity> gridEntities = new ArrayList<>();
 
     private Grid() {
         
@@ -27,13 +29,57 @@ public class Grid {
     }
 
     public void addElement(TrackElement element) {
-        gridElements.add(element);
+        draw(element);
     }
 
-    public void draw() {
+    public void removeEntityAtPos(Point2D pos) {
+        for(Entity e : gridEntities) {
+            boolean onX = e.getX() <= pos.getX() && e.getX() + GRID_SIZE >= pos.getX();
+            boolean onY = e.getY() <= pos.getY() && e.getY() + GRID_SIZE >= pos.getY();
+            if(onX && onY) {
+                gridEntities.remove(e);
+                FXGL.getGameWorld().removeEntity(e);
+
+                int x = (int) pos.getX() / GRID_SIZE;
+                int y = (int) pos.getY() / GRID_SIZE;
+                removeElementAtPos(x, y);
+                break;
+            }
+        }
+    }
+
+    private void removeElementAtPos(int x, int y) {
+        for(TrackElement e : gridElements) {
+            if(e.X_LOC == x && e.Y_LOC == y) {
+                gridElements.remove(e);
+                break;
+            }
+        }
+    }
+
+    public void drawAll() {
         for(TrackElement te : gridElements) {
             Entity e = FXGL.entityBuilder().at(te.X_LOC * GRID_SIZE, te.Y_LOC * GRID_SIZE).view(te.getImageString()).buildAndAttach();
-            e.setScaleX(1);
+            gridEntities.add(e);
         }
+    }
+
+    private void draw(TrackElement element) {
+        if(elementExistsAtPos(element.X_LOC, element.Y_LOC)) {
+            
+        } else {
+            gridElements.add(element);
+            Entity e = FXGL.entityBuilder().at(element.X_LOC * GRID_SIZE, element.Y_LOC * GRID_SIZE).view(element.getImageString()).buildAndAttach();
+            gridEntities.add(e);
+        }
+    }
+
+    private boolean elementExistsAtPos(int x, int y) {
+        for(TrackElement te : gridElements) {
+            if(te.X_LOC == x && te.Y_LOC == y) {
+                return true;
+            }
+        }
+        return false;
     }
 }

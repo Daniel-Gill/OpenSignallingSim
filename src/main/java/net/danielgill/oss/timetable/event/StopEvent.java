8f970d7707f1d6d653fa721ebdc7137b00319c39
Daniel-Gill <d.gill@se20.qmul.ac.uk>
2022-05-11 22:18:18 +0100

    Added more details to stop event

--- src/main/java/net/danielgill/oss/timetable/event/StopEvent.java
+++ src/main/java/net/danielgill/oss/timetable/event/StopEvent.java
@@ -1,11 +1,28 @@
 package net.danielgill.oss.timetable.event;
 
+import net.danielgill.oss.location.Location;
 import net.danielgill.oss.time.Time;
 
 public class StopEvent extends Event {
+    Time depTime;
+    Location loc;
 
-    public StopEvent(Time time, EventType type) {
-        super(time, type);
+    public StopEvent(Time arrTime, Time depTime, Location loc) {
+        super(arrTime, EventType.STOP);
+        this.depTime = depTime;
+        this.loc = loc;
+    }
+
+    public Time getArrTime() {
+        return this.time;
+    }
+
+    public Time getDepTime() {
+        return this.depTime;
+    }
+
+    public Location getLocation() {
+        return this.loc;
     }
     
 }
